da1589d6ad3c0aea17c7561c19037b4c3fe4cb10
Cedric Dessez <cedric@dessez.fr>
2014-08-06 17:09:07 +0100

    Make the server print the throughput as well, and intercept CTRL+C to close the connection in a proper way

--- cloremwebserver/cloremwebserver.c
+++ cloremwebserver/cloremwebserver.c
@@ -6,8 +6,14 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+
+static int keep_running = 1;
+static int sockfd = -1;
 
 void dostuff(int);
+void *refresh_display(void *data_counter);
 
 void error(char *msg)
 {
@@ -15,9 +21,25 @@ void error(char *msg)
   exit(1);
 }
 
+void int_handler_parent(int dummy)
+{
+  if(keep_running <= 0){
+    close(sockfd);
+    exit(0);
+  }
+  keep_running--;
+  signal(SIGINT, int_handler_parent);
+}
+
+void int_handler_child(int dummy)
+{
+  keep_running = 0;
+}
+
+
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, pid;
+  int newsockfd, portno, pid;
   struct sockaddr_in serv_addr, cli_addr;
   socklen_t clilen;
 
@@ -39,6 +61,7 @@ int main(int argc, char *argv[])
   clilen = sizeof(cli_addr);
   while (1){
     newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    signal(SIGINT, int_handler_parent);
     if (newsockfd < 0)
       error("ERROR on accept");
     pid = fork();
@@ -48,10 +71,12 @@ int main(int argc, char *argv[])
       close(sockfd);
       dostuff(newsockfd);
       exit(0);
-    } else
+    } else {
       close(newsockfd);
-  }                             /* end of while */
-  return 0;                     /* we never get here */
+      keep_running = 2;
+    }
+  }
+  return 0;
 }
 
 
@@ -60,9 +85,20 @@ void dostuff(int sock)
   int n, i, html_size;
   //int ii, html_nrows, *html_rows_i;
   int wcount, written, to_write;
+  long total_snd = 0;
   char rbuffer[1500], raw_html[100000], *ptr;
   FILE *fp;
   char *header = "HTTP/1.0 200 OK\nContent-Type: text/html\n\n<html><body>\n";
+  pthread_t display_th;
+  int call_ret;
+
+  signal(SIGINT, int_handler_child);
+
+  /* Creates the new display thread */
+  call_ret = pthread_create(&display_th, NULL, refresh_display, 
+                              (void *)(&total_snd));
+  if (call_ret)
+    error("ERROR creating a new thread");
 
   /* Read the random html from the file html_random.txt */
   fp = fopen("html_random.txt", "r");
@@ -92,12 +128,13 @@ void dostuff(int sock)
   n = read(sock, rbuffer, 1500);
   if (n < 0)
     error("ERROR reading from socket");
-  printf("Here is the message: %s\n", rbuffer);
+  printf("\n=======================================================\n");
+  printf("New connection:\n");
 
   /* Send http header, html header, 
    * and then keep sending random data forever */
   write(sock, header, 54);
-  while(1){
+  while(keep_running){
     written = 0;
     ptr = raw_html;
     to_write = html_size;
@@ -112,5 +149,39 @@ void dostuff(int sock)
       ptr += wcount;
       to_write -= wcount;
     }
+    total_snd += written;
   }
+  printf("\nReceived a SIGINT. Closing...");
+  close(sock);
+} 
+
+
+void *refresh_display(void *data_counter)
+{
+  time_t cur_time, init_time, prev_time;
+  unsigned long *counter, prev_count, count, count_mb;
+  double avg_rate, rate;
+  counter = (unsigned long *)data_counter;
+  prev_count = 0;
+  time(&init_time);
+  prev_time = init_time;
+  sleep(1);
+  while(keep_running){
+    time(&cur_time);
+    count = *counter;
+    count_mb = count >> 20;
+    rate = (double)((count - prev_count) * 8)
+            / (double)(cur_time - prev_time);
+    avg_rate = (double)(count * 8) / (double)(cur_time - init_time);
+    printf("\rTot=  %ld MB / %ld s     Avg=  %ld Mbps     Inst=  %ld Mbps", 
+        count_mb, cur_time - init_time, 
+        (long)avg_rate >> 20, (long)rate >> 20);
+    fflush(stdout);
+    prev_count = count;
+    prev_time = cur_time;
+    sleep(1);
+  }
+
+  pthread_exit(NULL);
 }
+
